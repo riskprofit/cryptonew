9790d82ed1b524ebc23c215380808779582b58c4
Talisman Group <Talisman Group@DESKTOP-OU0UQCA>
2017-08-07 20:05:01 +0300

    Добавьте файлы проекта.

---
+++ master/src/p2p/p2p_networks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2015 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace nodetool
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x41, 0x45, 0x01, 0x41, 0x41, 0x00, 0x01, 0x01, 0x45, 0x41, 0x05, 0x42, 0x45, 0x41, 0x01, 0x40 } };
+}
